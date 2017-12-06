0587a8fd13cc58d694290bf390f3b03426561e08
jleugeri <jleugeri@uos.de>
2017-12-06 19:18:58 +0100

    working example?

--- code/bci_grid/experiment.py
+++ code/bci_grid/experiment.py
@@ -4,13 +4,15 @@ from twisted.internet import reactor, defer, task
 import itertools
 import random
 
+# This module-level variable (buh!) keeps track of the current state of the experiment
+state = {}
 
 def char2idx(char, grid_shape):
     """ Finds a symbol in the symbol list and converts its position to grid coordinates"""
     symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ !?."
     idx = symbols.index(char.upper())
     assert idx >= 0, "Character '{}' not in the list of allowed symbols ({})".format(char, symbols)
-    return np.unravel_index(idx, grid_shape)
+    return np.unravel_index(idx, grid_shape), idx
 
 
 # This is an asynchroneous co-routine for running the experiment itself
@@ -28,12 +30,12 @@ async def experiment(targets=None, max_runs=None, repetitions=1, flash_time=0.4)
                             For each repetition, each column and row of the grid will be highlighted exactly once.
         flash_time (float): Time in seconds of each flashing each row/column. Defaults to 0,4s.
     """
-    global experiment_state
+    global state
     width,height = boilerplate()
 
     # Create 5x6 grid of symbols
     grid_shape = (5,6)
-    g = Grid(*grid_shape, "bci/symbols.png", img_rows=5, img_cols=6, coords=((-0.75,0.5), (0.75, -0.5)), space=0.5)
+    g = Grid(*grid_shape, "bci_grid/symbols.png", img_rows=5, img_cols=6, coords=((-0.75,0.5), (0.75, -0.5)), space=0.5)
 
     if targets is None:
         targets = itertools.repeat(None)
@@ -70,15 +72,15 @@ async def experiment(targets=None, max_runs=None, repetitions=1, flash_time=0.4)
         render(False)
 
         # right now, there is neither a stimulus nor targets
-        experiment_state["flashed"] = flashed
-        experiment_state["target"] = None
+        state["highlighted"] = flashed
+        state["target"] = None
         await async_sleep(2)
 
         # Set symbol to focus, if any
         if target is not None:
-            char_idx = char2idx(target, grid_shape)
+            char_idx2d, char_idx = char2idx(target, grid_shape)
             # Show the desired character
-            g.symbols[char_idx[0], char_idx[1]].color=np.array([1.0,0.0,0.0,1.0])
+            g.symbols[char_idx2d[0], char_idx2d[1]].color=np.array([1.0,0.0,0.0,1.0])
 
         # draw grid w/o any highlights
         render()
@@ -114,8 +116,8 @@ async def experiment(targets=None, max_runs=None, repetitions=1, flash_time=0.4)
                 render()
 
                 # record stimulus and target
-                experiment_state["flashed"] = flashed
-                experiment_state["target"] = None if not target else char_idx
+                state["highlighted"] = flashed
+                state["target"] = None if not target else char_idx
 
                 # sleep a while
                 await async_sleep(flash_time)
@@ -124,7 +126,7 @@ async def experiment(targets=None, max_runs=None, repetitions=1, flash_time=0.4)
 
         # Release focused symbol, if any
         if target is not None:
-            g.symbols[char_idx[0], char_idx[1]].color=np.array([1.0,1.0,1.0,0.25])
+            g.symbols[char_idx2d[0], char_idx2d[1]].color=np.array([1.0,1.0,1.0,0.25])
 
     # The experiment is done!
     pygame.quit()
--- code/bci_grid/train.py
+++ code/bci_grid/train.py
@@ -1,25 +1,50 @@
 from utils import *
 from Traumschreiber import *
 from twisted.internet import reactor, defer, task
+#from twisted.enterprise import adbapi
 import itertools
 import random
-from .experiment import experiment
+from .experiment import state, experiment
+import datetime
+import pandas
 
-# This global variable (buh!) keeps track of the current state of the experiment
-experiment_state = {}
+db_ready = False
+dbpool = None
+
+#3600s*250 = 900000 samples
+MAX_SAMPLES = 100000
+
+data_store = []
+data_idx = 0
 
 def data_callback(data_in):
-    print(experiment_state, data_in)
+    global data_idx
 
+    new_data = np.frombuffer(np.array(data_in, dtype=np.int8), dtype=np.dtype('<i2')).reshape((8,))
+    inrow =  np.hstack((datetime.datetime.now(), new_data, state["highlighted"].ravel(), state["target"]))
+    data_store.append(inrow)
+    data_idx += 1
+
+def data_save(ex):
+    print(data_store)
+    df = pandas.DataFrame(data_store, columns=
+        ["timestamp"] +
+        ["channel{}".format(i) for i in range(8)] +
+        ["highlighted{}".format(i) for i in range(30)]+
+        ["target"]).set_index("timestamp")
+
+    df.to_pickle("recording.pkl")
 
 async def run_experiment(addr, training_text="", **kwargs):
+    global db_ready
     async with Traumschreiber(addr=addr) as t:
         await t.start_listening(data_callback)
         await experiment(**kwargs)
+        db_ready = False
 
 def main(reactor):
-    #d = defer.ensureDeferred(experiment("HALLO WELT"))
-    d = defer.ensureDeferred(run_experiment(None, targets="HALLO WELT"))
-    return d
+    ex = defer.ensureDeferred(run_experiment(None, targets="HALLO WELT"))
+    ex.addCallback(data_save)
+    return ex
 
 task.react(main)
